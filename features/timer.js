df0f889849b97b83e9895de7af78df19ed260ab3
mmc27 <79273991+mmc27@users.noreply.github.com>
2022-12-05 23:03:10 -0600

    Add files via upload

---
+++ features/timer.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const ms = require('ms')
+
+module.exports = async (client, discord, member) => {
+    setInterval(async function() {
+        
+    }, ms('5m'))
+}
