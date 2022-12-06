f67dc6e0506c5a514278feab59fc6af438f21910
mmc27 <79273991+mmc27@users.noreply.github.com>
2022-12-05 23:02:09 -0600

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,36 @@
+const { Client, Intents, MessageEmbed, Permissions, MessageActionRow, Message, MessageButton } = require('discord.js')
+
+module.exports = {
+    category: 'Commands',
+    description: 'shows this message',
+    slash: 'both',
+    guildOnly: false,
+    testOnly: false,
+
+    callback: async ({ message, interaction: msgInt, channel }) => {
+        const help = new MessageEmbed()
+        .setColor('#ADD8E6')
+        .setAuthor('soar assistant v2')
+        .setDescription(
+        '`help` - shows this message' + '\n'
+        + '`prefix` - shows bot prefix' + '\n'
+        + '`invite` - invite people to the server' + '\n'
+        + '`suggest` - suggest a feature' + '\n'
+        + '`report` - report a problem' + '\n'
+        + '`search` - search our resources' + '\n'
+        + '`pomodoro` - create or manage a study channel' + '\n'
+        + '`resources` - useful third-party resources' + '\n')
+        .setTimestamp()
+
+        if (message) {
+            await message.reply({
+                embeds: [help],
+            })
+            return
+        }
+    
+        await msgInt.reply({
+            embeds: [help],
+        })
+    }
+}
