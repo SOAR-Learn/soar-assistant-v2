a475e0f5ac2e756d706b0a0e7c2a556b41e1f208
mmc27 <79273991+mmc27@users.noreply.github.com>
2022-12-23 10:01:42 -0600

    fix verification message

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
 const DiscordJS = require('discord.js')
 const WOKCommands = require('wokcommands')
 const path = require('path')
+const ms = require('ms')
 const dotenv = require('dotenv')
 dotenv.config()
 
-const ms = require('ms')
-
-
-// const firebase = require('firebase-admin')
-
-// const config = {
-//     apiKey: "AIzaSyCInDSiCzj1cQWB7h1cfisxLW2nyLZNXqI",
-//     authDomain: "soar-portal.firebaseapp.com",
-//     databaseURL: "https://soar-portal-default-rtdb.firebaseio.com",
-//     projectId: "soar-portal",
-//     storageBucket: "soar-portal.appspot.com",
-//     messagingSenderId: "328243213522",
-//     appId: "1:328243213522:web:b97e143845709f3375290b",
-//     measurementId: "G-CQ4PMWBYKC"
-// }
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./soar-portal-firebase-adminsdk-wmpt1-8da886742a.json");
+const admin = require('firebase-admin')
+const serviceAccount = require('./soar-portal-firebase-adminsdk-wmpt1-8da886742a.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://soar-portal-default-rtdb.firebaseio.com/"
+  databaseURL: 'https://soar-portal-default-rtdb.firebaseio.com/'
 });
 
 const udb = admin.database().ref("/users")
@@ -77,45 +61,50 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', async message => {
-    if (message.author.bot) return;
-    if (message.member.roles.cache.some(r => r.name === "Student")) return;
-    let student_role = message.guild.roles.cache.find(role => role.name === 'Student');
+    if (message.author.bot) return
 
-    const channel = message.guild.channels.cache.find(c => c.name === 'verification');
-    const content = message.content;
+    let student_role = message.guild.roles.cache.find(role => role.name === 'Student')
+    if (!student_role) return
 
-    const args = message.content.split(/ +/);
+    if (message.member.roles.cache.some(r => r.name === 'Student')) return
+
+    const channel = message.guild.channels.cache.find(c => c.name === 'verification')
+    if (!channel) return
+
+    const content = message.content
+
+    const args = message.content.split(/ +/)
     
-    if (message.channel != channel) return;
+    if (message.channel != channel) return
 
-    let users = [];
+    let users = []
     
     udb.orderByChild('first').equalTo(args[0]).once('value', snapshot => {
         if (snapshot.exists()) {
-            users.push(snapshot.exportVal());
+            users.push(snapshot.exportVal())
         }
     }).then(() => {
-    var userFound = false;
+    var userFound = false
     for (var i = 0; i < users.length; i++) {
         for (j in users[i]) {
-        console.log(j);
+        console.log(j)
         if (users[i][j].last == args[1]) {
-            console.log(`Exists: ${users[i].last}`);
-            message.channel.send('Welcome to SOAR!');
+            console.log(`Exists: ${users[i].last}`)
+            message.channel.send('Welcome to SOAR!')
             setTimeout(function() {
-                console.log(content);
-                message.member.setNickname(content);
+                console.log(content)
+                message.member.setNickname(content)
             }, ms('1s'))
             setTimeout(function() {
                 console.log("Author ID: " + message.author.id);
-                message.guild.members.cache.get(message.author.id).roles.add(student_role);
+                message.guild.members.cache.get(message.author.id).roles.add(student_role)
             }, ms('2s'))
-            userFound = true;
-            break;
+            userFound = true
+            break
         }}
     }
     if (userFound !== true) {
-        console.log(`Failed Verification: ${message.author.tag}`);
+        console.log(`Failed Verification: ${message.author.tag}`)
         
         let verification_message = `The name you entered was not found in the SOAR Database. Please register as a member by visiting https://portal.soarlearn.org and try again.`
         message.channel.send(verification_message)
