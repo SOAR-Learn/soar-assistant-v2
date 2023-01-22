c884eee614f75c5cd71d28af18e957652eec00f7
mmc27 <79273991+mmc27@users.noreply.github.com>
2023-01-22 13:39:07 -0600

    add try catch block

--- features/timer.js
+++ features/timer.js
@@ -4,23 +4,29 @@ const ref = require('../index.js').firebaseRef
 
 module.exports = async (client, discord, member) => {
     setInterval(async function() {
+      try {
         ref.orderByChild('voiceChannel').equalTo(true).once('value').then((snapshot) => {
-            snapshot.forEach((childSnapshot) => {
-              const childRef = childSnapshot.ref
-              const childData = childSnapshot.val()
+          snapshot.forEach((childSnapshot) => {
+            const childRef = childSnapshot.ref
+            const childData = childSnapshot.val()
 
-              let add = (childData.currency + Math.random() * 4 + 1)
+            let add = (childData.currency + Math.random() * 4 + 1)
 
-              let time = childData.studyTime + 1
+            let time = childData.studyTime + 1
 
-              childRef.update({
-                studyTime: time
-              })
+            childRef.update({
+              studyTime: time
+            })
 
-              childRef.update({
-                currency: Math.round(add)
-              })
+            childRef.update({
+              currency: Math.round(add)
             })
           })
+        })
+      }
+      catch (error) {
+        console.log('voiceChannel not found')
+      }
+        
     }, ms('1m'))
 }
